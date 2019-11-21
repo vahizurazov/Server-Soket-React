77ac3872ee5d5fcb2b45f281c9d982c835eddf32
vahizurazov <vagiz.damir@gmail.com>
2019-11-21 15:37:04 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:4001"
+    };
+  }
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("FromAPI", data => this.setState({ response: data }));
+  }
+  convertTemp = temp => {
+    return (5 / 9) * (temp - 32);
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    const { response } = this.state;
+    console.log(response);
+
+    return (
+      <div style={{ textAlign: "center" }}>
+        {response ? (
+          // <p>
+          //   The temperature in kharkiv is:{" "}
+          //   {this.convertTemp(response.currently.temperature).toFixed(2)} °C
+          // </p>
+          <div>
+            {response.columns.map(col => (
+              <div key={col.id}>
+                <p>{col.title}</p>
+                <p>{col.id}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
